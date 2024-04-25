b7ad269318e60c71486ffdb3ff918d49e7453e44
kyung512 <kyungsun.na@gmail.com>
2024-04-25 16:20:39 -0500

    dynamically change titles

--- main.js
+++ main.js
@@ -1,23 +1,44 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    console.log(queryData);
-    console.log(queryData.id);
-    if(_url == '/'){
-      _url = '/index.html';
-    }
-    if(_url == '/favicon.ico'){
-      response.writeHead(404);
-      response.end();
-      return;
-    }
-    response.writeHead(200);
-    // response.end(fs.readFileSync(__dirname + _url));
-    response.end(queryData.id);
-  
- 
+var app = http.createServer(function (request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var title = queryData.id;
+
+  if (_url == '/') {
+    title = 'Welcome';
+  }
+  if (_url == '/favicon.ico') {
+    response.writeHead(404);
+    response.end();
+    return;
+  }
+  response.writeHead(200);
+  var template = `
+    <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>WEB1 - ${title}</title>
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ol>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ol>
+          <h2>${title}</h2>
+          <p>
+            HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser. It defines the content and structure of web content. It is often assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.
+          </p>
+        </body>
+      </html>
+    `;
+  response.end(template);
+
+
 });
 app.listen(3000);
