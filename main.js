fc9848b9151df5decb0d6e6dd2ceafc393aeda02
kyung512 <kyungsun.na@gmail.com>
2024-04-26 15:08:34 -0500

    글 삭제 기능 완성

--- main.js
+++ main.js
@@ -60,8 +60,8 @@ var app = http.createServer(function (request, response) {
             `<a href="/create">create</a>
              <a href="/update?id=${title}">update</a>
              <form action="delete_process" method="post">
-             <input type="hidden" name="id" value="${title}">
-             <input type="submit" value="delete">
+              <input type="hidden" name="id" value="${title}">
+              <input type="submit" value="delete">
              </form> 
            `
           );
@@ -140,6 +140,20 @@ var app = http.createServer(function (request, response) {
         });
       });
     });
+  } else if (pathname === '/delete_process') {
+    var body = '';
+    request.on('data', function(data) {
+      body = body + data;
+    });
+    request.on('end', function() {
+      var post = qs.parse(body);
+      var id = post.id;
+      fs.unlink(`data/${id}`, function(error) {     
+        response.writeHead(302, {Location: `/`});
+        response.end();
+      })
+    });
+
   } else {
     response.writeHead(404);
     response.end('Not Found');
