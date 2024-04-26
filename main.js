3e2ab0e1a52f76b6fbc301503449be6c5c5bc27e
kyung512 <kyungsun.na@gmail.com>
2024-04-25 20:34:33 -0500

    read files: HTML CSS JavaScript

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
@@ -15,7 +16,8 @@ var app = http.createServer(function (request, response) {
     return;
   }
   response.writeHead(200);
-  var template = `
+  fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+    var template = `
     <!DOCTYPE html>
       <html lang="en">
         <head>
@@ -31,14 +33,13 @@ var app = http.createServer(function (request, response) {
             <li><a href="/?id=JavaScript">JavaScript</a></li>
           </ol>
           <h2>${title}</h2>
-          <p>
-            HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser. It defines the content and structure of web content. It is often assisted by technologies such as Cascading Style Sheets (CSS) and scripting languages such as JavaScript.
-          </p>
+          <p>${description}</p>
         </body>
       </html>
     `;
-  response.end(template);
-
+    response.end(template);
+  });
 
 });
+
 app.listen(3000);
