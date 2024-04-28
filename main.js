adc544819e171510ae5860d37351a200fb81ea23
kyung512 <kyungsun.na@gmail.com>
2024-04-28 09:25:52 -0500

    template를 module 로 전환

--- main.js
+++ main.js
@@ -3,40 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-// refactoring  
-template = {
-  HTML: function (list, title, body, control) {
-    return `
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>WEB1 - ${title}</title>
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        ${list}
-        ${control}
-        ${body}
-      </body>
-      </html>
-    `;
-  },
-  list: function (filelist) {
-    var list = '<ul>';
-    var i = 0;
-    while (i < filelist.length) {
-      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-      i = i + 1;
-    }
-    list = list + '</ul>';
-    return list;
-  }
-};
-
-
-
+var template = require('./lib/template.js');
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
